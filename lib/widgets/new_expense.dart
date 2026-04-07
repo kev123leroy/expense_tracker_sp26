4fa1cc6dff3c39401d1c6d4d63d8ffe79e41f0f3
kev123leroy <kevinp.leroy@gmail.com>
2026-04-07 12:15:55 -0400

    list

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,7 @@
+import 'package:expense_tracker_sp26/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget{
   const NewExpense({super.key});
@@ -10,39 +13,89 @@ class NewExpense extends StatefulWidget{
 class _NewExpenseState extends State<NewExpense>{
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year -1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context:context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,  
+    );
+    print(pickedDate);
+    setState((){
+      _selectedDate = pickedDate;
+    });
+  }
+
+  @override
+  void dispose(){
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.all(16),
+    return Padding(
+      padding: EdgeInsets.all(16),
       child: Column(
-        children: [
+        children:[
           TextField(
             controller: _titleController,
             maxLength: 50,
-            keyboardType:TextInputType.name,
-            decoration: const InputDecoration(
-              label: Text('Title'),
+            keyboardType: TextInputType.name,
+            decoration: InputDecoration(
+              label: Text("Title"),
             ),
           ),
-          
-            TextField(
-            controller: _amountController,
-            maxLength: 10,
-            keyboardType:TextInputType.number,
-            decoration: const InputDecoration(
-              prefixText: '\$',
-              label: Text('Amonut'),
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller:_amountController,
+                  maxLength: 10,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: '\$',
+                    label: Text("Amount"),
+                  ),
+                ),
+              ),
+              SizedBox(width:16),
+              Expanded(child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                Text(
+                  _selectedDate == null ?
+                  'Select Date' :
+                  formatter.format(_selectedDate!)
+                ),
+                IconButton(onPressed: _presentDatePicker, icon: const Icon(Icons.calendar_month))
+              ],),),
+            ],
           ),
-          
           Row(children: [
+            DropdownButton(
+              items: Category.values.map(
+                (category) => DropdownMenuItem(
+                  value: category,
+                  child: Text(category.name.toString(),),
+                ),
+              ).toList(),
+              onChanged: (value){}),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            }, child: Text("Cancel")),
             ElevatedButton(onPressed: (){
               print(_titleController.text);
               print(_amountController.text);
-            }, child: Text("save expense"))
+            }, child: Text("Save Expense")),
           ],)
-        ],
-        ),
-    );
+        ]
+      )
+  );
   }
 }
